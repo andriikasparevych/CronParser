17e82c1b98b40d8a6be05bee27911bc5ac755abe
Andrii Kasparevych <akasparevych@labster.com>
2021-11-21 18:23:44 +0100

    Add output formatting

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿namespace CronParser // Note: actual namespace depends on the project name.
+﻿namespace CronParser
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var parser = new CronParser();
-            var result = parser.Parse(args[0]);
+             var parser = new CronParser();
+             var result = parser.Parse(args[0]);
 
-            Console.WriteLine(result.ToString());
+             Console.WriteLine(FormatOutput(result));
+        }
+
+        private static string FormatOutput(CronParsingResult result) {
+            return $"{"minute", -20}{result.Minute}{Environment.NewLine}"
+                    + $"{"hour", -20}{result.Hour}{Environment.NewLine}"
+                    + $"{"day of month", -20}{result.DayOfMonth}{Environment.NewLine}"
+                    + $"{"month", -20}{result.Month}{Environment.NewLine}"
+                    + $"{"day of week", -20}{result.DayOfWeek}{Environment.NewLine}"
+                    + $"{"command", -20}{result.Command}{Environment.NewLine}";
         }
     }
 }
