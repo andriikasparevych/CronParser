c171632bfc9367e68808f48c5478cec02fe1a6a1
Andrii Kasparevych <akasparevych@labster.com>
2021-11-21 18:10:55 +0100

    Change Main method to handle input

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace CronParser // Note: actual namespace depends on the project name.
+﻿namespace CronParser // Note: actual namespace depends on the project name.
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser = new CronParser();
+            var result = parser.Parse(args[0]);
+
+            Console.WriteLine(result.ToString());
         }
     }
 }
